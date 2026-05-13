7e6ca9ad77b2f66ebf819613d96815ac0245a11c
Le-s-s <lexton.d.lewis1@gmail.com>
2026-05-13 09:42:48 -0600

    added button to call weather api

--- src/dom.js
+++ src/dom.js
@@ -2,7 +2,6 @@ import "./style.css";
 
 const dom = (function () {
   const createDom = function () {
-
     const body = document.querySelector("body");
 
     const container = document.createElement("div");
@@ -12,23 +11,37 @@ const dom = (function () {
     ui.classList.add("ui");
 
     const address = document.createElement("input");
-    address.type = "text"
+    address.type = "text";
 
     const button = document.createElement("button");
-    button.type = "button"
-    button.textContent = "Search"
+    button.type = "button";
+    button.textContent = "Search";
 
     const display = document.createElement("div");
     display.classList.add("display");
 
-    ui.appendChild(address)
-    ui.appendChild(button)
-    container.appendChild(ui)
-    container.appendChild(display)
+    ui.appendChild(address);
+    ui.appendChild(button);
+    container.appendChild(ui);
+    container.appendChild(display);
     body.appendChild(container);
   };
+  const watchInput = function (weatherJson) {
+    const button = document.querySelector("button");
+    const input = document.querySelector("input");
+    
+    console.log(input.textContent);
+    button.addEventListener("click", (event) => {
+      if (input !== "") {
+        return weatherJson(input.value);
+      } else {
+        return Error;
+      }
+      
+    });
+  };
 
-  return { createDom };
+  return { createDom, watchInput };
 })();
 
 export default dom;
