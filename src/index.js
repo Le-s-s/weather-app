d704d6cdb5f238e568ec980dbbc135e749191cf0
Le-s-s <lexton.d.lewis1@gmail.com>
2026-05-13 09:43:18 -0600

    Added api function and button listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import dom from "./dom.js"
+import handler from "./handler.js"
 
 
 addEventListener("DOMContentLoaded", (event) => {
-  dom.createDom();
+  handler.start()
+
 });
+
+// create dom with input and search button
+// listen for button press
+// when button pressed check text input
+// if input not empty, and is zip code
+// put zip code in weather api returning json information
+// read information and return legible
+// make visual changes based on information.
+
+//additional note
+// use async function
