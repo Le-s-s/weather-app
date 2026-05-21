580feb6d2b89883b08f44d9b67b86d6d72c1d1c9
Le-s-s <lexton.d.lewis1@gmail.com>
2026-05-21 11:42:23 -0600

    Temperature based styling

--- src/handler.js
+++ src/handler.js
@@ -7,8 +7,8 @@ const handler = (function () {
     dom.watchInput(async (zipCode) => {
       const apiObj = await api.weatherJson(zipCode);
       const tempData = api.findData(apiObj);
-      const displayData = new api.dataObject(tempData)
-      console.log(displayData)
+      const displayData = new api.dataObject(tempData);
+      console.log(displayData);
       const display = document.querySelector(".display");
       display.innerHTML = "";
       dom.injectApi(displayData);
